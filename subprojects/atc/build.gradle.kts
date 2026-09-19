b78b23ee508fca736a9b2d440d68a24225aa7df1
Obieda <bis.2410423@eia.edu.eg>
2026-09-19 21:52:44 +0300

    fix(atc): align minSdk with AndroidX Core 1.18

--- subprojects/atc/build.gradle.kts
+++ subprojects/atc/build.gradle.kts
@@ -26,7 +26,7 @@ plugins {
 android {
     namespace = "${BuildConfig.PACKAGE_NAME}.atc"
     
-    defaultConfig { minSdk = 21 }
+    defaultConfig { minSdk = 23 }
     
     buildFeatures {
         buildConfig = false
